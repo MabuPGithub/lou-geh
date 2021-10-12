c94760e2827cff8c1fd9b5914848b35cb9d342ad
MabuPGithub <mylespison@yahoo.com>
2021-10-12 20:42:56 +0800

    added suppliers CRUD

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,49 @@
+class SuppliersController < ApplicationController
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def show
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.unique_code(supplier_params)
+
+    if @supplier.save
+      redirect_to @supplier
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def update
+    @supplier = Supplier.find(params[:id])
+
+    if @supplier.update(supplier_params)
+      redirect_to @supplier
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @supplier = Supplier.find(params[:id])
+    @supplier.destroy
+
+    redirect_to '/suppliers'
+  end
+
+  private
+    def supplier_params
+      params.require(:supplier).permit(:unique_code, :name, :contact_number, :address)
+    end
+end
